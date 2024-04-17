1d81e1992830722a5d826233ee9548a00d042b92
Votinova <138308608+Votinova@users.noreply.github.com>
2024-04-17 18:19:16 +0700

    Create ClosuresandScopes.js

---
+++ tasks/ClosuresandScopes.js
@@ -0,0 +1,31 @@
+
+/*
+We want to create a function, which returns an array of functions, which return their index in the array. For better understanding, here an example:
+
+var callbacks = createFunctions(5); // create an array, containing 5 functions
+
+callbacks[0](); // must return 0
+callbacks[3](); // must return 3
+
+We already implemented that function, but when we actually run the code, the result doesn't look like what we expected. Can you spot, what's wrong with it? A test fixture is also available
+Мы хотим создать функцию, которая возвращает массив функций, которые возвращают свой индекс в массиве. Для лучшего понимания приведу пример:
+
+var callbacks = createFunctions(5); // create an array, containing 5 functions
+
+callbacks[0](); // must return 0
+callbacks[3](); // must return 3
+
+Мы уже реализовали эту функцию, но когда мы фактически запускаем код, результат выглядит не так, как мы ожидали. Можете ли вы определить, что с ним не так? Также имеется испытательное приспособление
+
+*/
+function createFunctions(n) {
+  let callbacks = [];
+
+  for (let i=0; i < n; i++) {
+    callbacks.push(function() {
+      return i;
+    });
+  }
+  
+  return callbacks;
+}
